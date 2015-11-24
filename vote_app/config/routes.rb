9584e452afadaadd4a8759050dd0ca54d7d58f40
HenryKpano <henry.kpano@meltwater.org>
2015-11-24 19:25:51 +0000

    Add user log in

--- vote_app/config/routes.rb
+++ vote_app/config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   end
   resources :votes
   resources :comments
+  resources :landings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
